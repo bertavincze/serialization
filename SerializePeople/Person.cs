b6cff3f3d658ad9a959c86e78739d5850f472aee
Berta Vincze <berta.vincze@gmail.com>
2019-07-29 10:38:31 +0200

    Add project files

---
+++ SerializePeople/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializePeople
+{
+    [Serializable]
+    class Person : IDeserializationCallback, ISerializable
+    {
+        [NonSerialized]
+        public int age;
+
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        internal Genders Gender { get; set; }
+
+        public Person(string name, DateTime dateOfBirth, Genders gender)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            CalculateAge();
+        }
+
+        private void CalculateAge()
+        {
+            age = (DateTime.Today).Year - DateOfBirth.Year;
+        }
+
+        public void Serialize(string output)
+        {
+            File.Create(output);
+            Stream stream = File.Open(output, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, this);
+            stream.Close();
+        }
+
+        public void DeSerialize(string output)
+        {
+            Stream stream = new FileStream(output, FileMode.Open, FileAccess.Read);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Person p = (Person)formatter.Deserialize(stream);
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Gender);
+            Console.WriteLine(p.age);
+            Console.ReadKey();
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + Name + " " + DateOfBirth + " " + Gender;
+        }
+
+        public void OnDeserialization(object sender)
+        {
+            age = (DateTime.Today).Year - DateOfBirth.Year;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Date of birth", DateOfBirth);
+            info.AddValue("Gender", Gender);
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            DateOfBirth = (DateTime)info.GetValue("Date of birth", typeof(DateTime));
+            Gender = (Genders)Enum.Parse(typeof(Genders), info.GetString("Gender"));
+        }
+
+        public enum Genders
+        {
+            male = 0,
+            female = 1
+        }
+    }
+}
---
+++ SerializePeople/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace SerializePeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Example", DateTime.Now, Person.Genders.female);
+            p.Serialize(@"C:\Users\Tulajdonos\Desktop\temp.txt");
+            p.DeSerialize(@"C:\Users\Tulajdonos\Desktop\temp.txt");
+
+            string fileName = @"C:\Users\Tulajdonos\Desktop\tempFile.txt";
+
+            IFormatter formatter = new BinaryFormatter();
+
+            SerializeItem(fileName, formatter);
+            DeserializeItem(fileName, formatter);
+            Console.WriteLine("Serialization complete.");
+            Console.ReadKey();
+        }
+
+        public static void SerializeItem(string fileName, IFormatter formatter)
+        {
+            Person person = new Person("Example", DateTime.Parse("1980.10.10."), Person.Genders.male); 
+
+            FileStream stream = new FileStream(fileName, FileMode.Create);
+            formatter.Serialize(stream, person);
+            stream.Close();
+        }
+
+
+        public static void DeserializeItem(string fileName, IFormatter formatter)
+        {
+            FileStream s = new FileStream(fileName, FileMode.Open);
+            Person person = (Person)formatter.Deserialize(s);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.DateOfBirth);
+            Console.WriteLine(person.Gender);
+            Console.WriteLine(person.age);
+        }
+    }
+}
